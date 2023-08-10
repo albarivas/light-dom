01a07bc87546a21e5faf699d2642e8d36feb02a4
Alba Rivas <arivas@salesforce.com>
2023-08-10 18:16:57 +0200

    static resources

---
+++ force-app/main/default/staticresources/myJSResource.js
@@ -0,0 +1,5 @@
+function changeMascot() { 
+  const mascotGreeting = document.querySelector("p.mascotGreeting");
+  // Shouldn't do this, but it's just an example!
+  mascotGreeting.innerHTML = `Hello Ruth!`;
+}
